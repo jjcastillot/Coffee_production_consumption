c31544278c644892de035418c6501ed46fbd09cf
jjcastillot <jjcastillot@gmail.com>
2023-05-04 11:56:52 -0500

    Updated dashboard and created reqs txt

--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import plotly.express as px
+import plotly.graph_objects as go
 from countries_dataset import countries
 from codes import country_codes
 from country_surface_area import country_areas
 from country_latitudes import latitudes
-sns.set_theme()
 st.set_page_config(layout='wide', initial_sidebar_state='expanded',page_title='☕Coffee around the world☕')
 st.write('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
 with open('style.css') as f:
@@ -27,7 +26,7 @@ def graph_title(col):
         case 'Imports':
             return f'{col} - Total 60kg bags in thousands'
         case 'Production Over Area':
-            return f'{col} - Total Production(K 60kg bags) over area in sq km2'
+            return f'{col} - Total Production(K 60kg bags) over area in 1000 sq km2'
         case 'Dom. Consumption Ratio':
             return f'{col} - Domestic Consumption(K 60kg bags) over total production'
         case _:
@@ -42,16 +41,13 @@ def prod_plot(df,column,color_scale):
         hover_name='Country',
         
         color_continuous_scale=color_scale,
-        #range_color=(0, 1500000),
         scope='world',
     )
-
     # Update the layout of the figure
     fig.update_layout(
         title=graph_title(column),
-        margin={'r': 0, 't': 40, 'l': 0, 'b': 10},
+        margin={'t':30},
     )
-
     # Show the figure
     return fig
 
@@ -62,13 +58,12 @@ def imports_plot(color_scale):
         color='Totals',
         hover_name='Country',
         color_continuous_scale=color_scale,
-        #range_color=(0, 1500000),
         scope='world',
     )
     # Update the layout of the figure
     fig.update_layout(
         title=graph_title('Imports'),
-        margin={'r': 0, 't': 20, 'l': 0, 'b': 0},
+        margin={'t': 30},
     )
     # Show the figure
     return fig
@@ -76,22 +71,34 @@ def imports_plot(color_scale):
 def create_world_plot(chart_option):
     match chart_option:
         case 'Production':
-            return prod_plot(df_totals,chart_option,'Greens')
+            return prod_plot(df_totals,chart_option,'Blues')
         case 'Domestic Consumption':
-            return prod_plot(df_totals,chart_option,'Reds')
+            return prod_plot(df_totals,chart_option,'Blues')
         case 'Gross Openings':
-            return prod_plot(df_totals,chart_option,'Purples')
+            return prod_plot(df_totals,chart_option,'Blues')
         case 'Exports':
             return prod_plot(df_totals,chart_option,'Blues')
         case 'Imports':
-            return imports_plot('Oranges')
+            return imports_plot('Blues')
         case 'Production Over Area':
-            return prod_plot(df_totals,chart_option,'Greens')
+            return prod_plot(df_totals,chart_option,'Blues')
         case 'Dom. Consumption Ratio':
-            return prod_plot(df_totals,chart_option,'Reds')
+            return prod_plot(df_totals,chart_option,'Blues')
         case _:
-            return prod_plot(df_totals,chart_option,'Greens')
-                
+            return prod_plot(df_totals,chart_option,'Blues')
+        
+def top10s(column):
+    if column=='Imports':
+        top10 = df_imports[['Country','Code','Totals']].sort_values(by='Totals', ascending=False).head(10)
+        top10 = top10.sort_values(by='Totals', ascending=True)
+        top10.columns = ['Country','Code','Imports']
+        return top10
+    else:
+        top10 = df_totals[['Country','Code',column]].sort_values(by=column, ascending=False).head(10)
+        top10 = top10.sort_values(by=column, ascending=True)
+        return top10
+
+            
 ######################################################################################################
 
 #Import excel files after initial row cleaning
@@ -135,18 +142,33 @@ df_totals['Latitude']=df_totals['Country'].map(latitudes)
 # Update data loading
 #data_load_state.text('Loading data...done!')
 
-
+######################################################################################
 
 # Create a sidebar for the streamlit dashboard
 st.sidebar.header('☕Coffee around the world☕')
 # Select one of the charts to print
 chart_option = st.sidebar.selectbox(
     'Please select one of the charts below:',('Production','Production Over Area','Domestic Consumption','Dom. Consumption Ratio','Gross Openings','Exports','Imports'))
-my_fig=create_world_plot(chart_option)
+# Creates selectbox for the line chart
+if chart_option == 'Imports':
+    line_option = st.sidebar.selectbox(
+        'See imports behavior of this country:',df_imports['Country']
+    )
+else:
+    line_option = st.sidebar.selectbox(
+        'See production behavior of this country:',df_totals['Country']
+    )    
 #Create columns of the dashboard
-col1,col2 = st.columns([4,1])
+col1,col2 = st.columns([3,1])
+my_fig=create_world_plot(chart_option)
 col1.plotly_chart(my_fig,use_container_width=True)
-#col2.top10_chart(chart_option)
+top10_chart=top10s(chart_option)
+col2.plotly_chart(go.Figure(go.Bar(y=top10_chart['Country'],x=top10_chart[chart_option],orientation='h')).update_layout(title=graph_title('Top 10 countries'),margin={'t':30}),
+                  use_container_width=True)
+# Creates a lineplot depending on the option selected
+
+
+# Shows the data summarized
 st.markdown('Check the data here:')
 with st.expander('Exporting countries consolidated data'):
     st.dataframe(df_totals.style.highlight_max(axis=0))
